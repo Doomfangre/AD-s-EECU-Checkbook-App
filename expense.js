f0aeda1dd85be633dbcb1949139a81dfeb0660a4
Doomfangre <Doomfangre@gmail.com>
2026-03-13 10:41:32 -0700

    More or less
    Just JS

--- expense.js
+++ expense.js
@@ -1,3 +1,82 @@
+document.getElementById("careerSelect").addEventListener("change", (eventy) => {
+  const salary = eventy.target.value;
+  const takeIn = tax(salary);
+  alert(takeIn);
+});
+
+document.getElementById("nekst").addEventListener("click", () => {
+    const para = document.getElementById("ppap");
+    para.innerHTML = document.getElementById("account").innerHTML;
+    console.log(para);
+});
+
+// Tax function
+function tax(sally) {
+  const sold = sally;
+  sally = sally - ((1.45 / 100) * sally);
+  sally = sally - ((6.2 / 100) * sally);
+  if (sold <= 12401) {
+    sally = sally - ((10 / 100) * sally);
+  } else if (sold >= 50400) {
+    sally = sally - ((22 / 100) * sally);
+  } else {
+  sally = sally - ((12 / 100) * sally);
+  }
+  sally = sally - ((4 / 100) * sally);
+  sally = Math.round(sally * 100) / 100;
+  return sally;
+}
+
+//grabbing career information and adding to dropdown
+async function getCareers() {
+  const url = "https://eecu-data-server.vercel.app/data";
+  try {
+      const response = await fetch(url);
+      const jobs = await response.json();
+      createOptions(jobs);
+      console.log("initiated");
+      return jobs;
+  }
+  catch (error) {
+      console.error("Error fetching careers data:", error);
+      return [];
+  }
+
+}
+
+//creates options for dropdowns & adds Event Listers (needs to be fixed)
+function createOptions(careers) {
+  const dropdown = document.getElementById("careerSelect");
+  careers.forEach((career, index) => {
+
+      const options = document.createElement("option");
+      options.innerHTML = `${career.Occupation}: ${career.Salary.toLocaleString()}`;
+      options.setAttribute("id", `${index}`);
+      options.setAttribute("value", `${career.Salary}`);
+      options.addEventListener("click", () => {
+          careerTitle.innerHTML = `Future Career: ${career.Occupation}`;
+      });
+      dropdown.appendChild(options);
+  });
+}
+
+getCareers(); //initating dropdown creation
+
+// function getMonthlyIncome(monthlyIncome) {
+//     const salary = career.Salary.toLocaleString; //grabbing salary from dropdown, needs to be fixed
+//     monthlyIncome = salary / 12;
+//     options.addEventListener("click", getMonthlyIncome);
+//     return monthlyIncome;
+// }
+
+// console.log(getMonthlyIncome());
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 const openDropdown = document.querySelectorAll(".openCategory");
 const categories = document.querySelectorAll(".expenseContainer");
 
@@ -47,5 +126,4 @@ function doughnutte(one, two, three, four, five) {
         }
       }
     };
-  }
-  
+}
--- greenAndInbetween.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("careerSelect").addEventListener("change", () => {
-    const salary = document.getElementsByTagName("option");
-    console.log(salary);
-});
-
