98aea050578bf904cd3c56d879ce730c79175aeb
Doomfangre <Doomfangre@gmail.com>
2026-03-15 20:37:15 -0700

    Okay

--- pie.js
+++ pie.js
@@ -12,10 +12,38 @@ let currentChart = null;
 document.getElementById("but").addEventListener("click", () => {
     const canvas = document.getElementById("chartCanvas");
     const house = document.getElementById("bill1").innerHTML;
-    const essen = document.getElementById("bill2").innerHTML;
-    const student = document.getElementById("bill3").innerHTML;
-    const life = document.getElementById("bill4").innerHTML;
-    const future = document.getElementById("bill5").innerHTML;
+    let essen = document.getElementById("bill2");
+    let student = document.getElementById("bill3");
+    let life = document.getElementById("bill4");
+    let future = document.getElementById("bill5");
+    if (document.getElementById("bill2") == null) {
+      essen = 0;
+      console.log("nope");
+    } else {
+      essen = document.getElementById("bill2").innerHTML;
+    }
+    if (document.getElementById("bill3") == null) {
+      student = 0;
+      console.log("nope");
+    } else {
+      student = document.getElementById("bill3").innerHTML;
+    }
+    if (document.getElementById("bill4") == null) {
+      life = 0;
+      console.log("nope");
+    } else {
+      life = document.getElementById("bill4").innerHTML;
+    }
+    if (document.getElementById("bill5") == null) {
+      future = 0;
+      console.log("nope");
+    } else {
+      future = document.getElementById("bill5").innerHTML;
+    }
+    
+    if (essen == null) {
+      console.log("aaaaaaaaaa");
+    }
     // Destroy old chart if it exists (common Chart.js gotcha)
     if (currentChart) currentChart.destroy();
   
